d0f7a2dd44dfc2ad3a2f443e77eece1a098669af
Damon-Hansen <95259338+Damon-Hansen@users.noreply.github.com>
2022-05-28 10:18:12 -0500

    updated styles to header and footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import './style.css';
 
 const Footer = () => {
   return (
-    <footer className="w-100 mt-auto bg-secondary p-4">
-      {/* <hr class="solid"></hr> */}
+    <footer className="w-100 mt-auto bg-secondary p-4 footer">
       <div className="container">
         &copy;{new Date().getFullYear()} by GamerLabs
       </div>
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './style.css';
 import Auth from '../../utils/auth';
 
 const Header = () => {
@@ -10,7 +10,7 @@ const Header = () => {
   };
 
   return (
-    <header className="bg-secondary mb-4 py-2 flex-row align-center">
+    <header className="bg-secondary mb-4 py-2 flex-row align-center header">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <Link to="/">
           <h1>GamerLabs</h1>
