2c3d49a0b3176b32db73bd95ccbf048b5162afbc
Damon-Hansen <95259338+Damon-Hansen@users.noreply.github.com>
2022-05-28 00:02:06 -0500

    updated footer and thoughts styles

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,8 +3,9 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
+      {/* <hr class="solid"></hr> */}
       <div className="container">
-        &copy;{new Date().getFullYear()} by Lernantino
+        &copy;{new Date().getFullYear()} by GamerLabs
       </div>
     </footer>
   );
